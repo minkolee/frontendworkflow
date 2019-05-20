b9ca73a2e4724b8d10023609a4a1946d5215973b
minkolee <minkolee@gmail.com>
2019-05-20 16:56:57 +0800

    再试试，可能缺少babel的配置文件
    去掉babel的部分能工作

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
         filename: "App.js"
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                use:["babel-loader"],
+                exclude: /node_modules/
+            }
+        ]
     }
 };
